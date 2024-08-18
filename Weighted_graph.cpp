b1b403e7dbfbc5c06868fc541c8345675bc68f0b
Sumit Garg <118594912+SumitGarg11@users.noreply.github.com>
2024-08-18 06:00:47 -0700

    input_Weighted_graph.cpp

--- Weighted_graph.cpp
+++ Weighted_graph.cpp
@@ -1,33 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Graph{
-  public:
-  unordered_map<int, list<pair<int,int>>> adjList;
-  
-  void addEdge(int u, int v,int wt,  bool dir){
-      if(dir == 1){
-         adjList[u].push_back({v, wt});
-      }
-      else{
-          adjList[u].push_back({v,wt});
-          adjList[v].push_back({u,wt});
-      }
-  }
-  void printAdjList(){
-      for(auto i : adjList){
-          cout<<i.first<<" : { "; // source Node;
-          for(pair<int,int> p : i.second){
-              cout<<" { "<< p.first<<" ,  " << p.second<<" }  ";
-              // 1 : {  {2 ,5} , {}, };
-          }
-          cout<<" } "<<endl;
-      }
-  }
+
+class Graph {
+public:
+    unordered_map<int, list<pair<int, int>>> adjList;
+
+    void addEdge(int u, int v, int wt, bool dir) {
+        // Add edge from u to v with weight wt
+        adjList[u].push_back({v, wt});
+        if (!dir) {
+            // Add edge from v to u with weight wt (if undirected)
+            adjList[v].push_back({u, wt});
+        }
+    }
+
+    void printAdjList() {
+        for (const auto& i : adjList) {
+            cout << i.first << " : { "; // Source Node
+            for (const auto &p : i.second) {
+                cout << " { " << p.first << " , " << p.second << " } ";
+            }
+            cout << " }" << endl;
+        }
+    }
 };
-int main(){
+
+int main() {
     Graph g;
-    g.addEdge(1,3,30, 0);
-    g.addEdge(3,2,40,0);
-    g.addEdge(2,5,60,0);
+    
+    int V, E;
+    cout << "Enter the number of vertices: ";
+    cin >> V;
+    
+    cout << "Enter the number of edges: ";
+    cin >> E;
+    
+    bool dir;
+    cout << "Enter 1 for directed or 0 for undirected: ";
+    cin >> dir;
+    
+    cout << "Enter the edges (u v wt):" << endl;
+    for (int i = 0; i < E; ++i) {
+        int u, v, wt;
+        cin >> u >> v >> wt;
+        g.addEdge(u, v, wt, dir);
+    }
+    
+    cout << "Adjacency List:" << endl;
     g.printAdjList();
+    
+    return 0;
 }
